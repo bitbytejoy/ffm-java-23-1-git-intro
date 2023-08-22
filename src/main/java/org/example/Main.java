bf462cf66a775542bf6dd5a3281463bdd059c081
bitbytejoy <bitbytejoy@users.noreply.github.com>
2023-08-22 14:41:53 +0200

    Update Main.java
    Changed on the server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world update!");
+        System.out.println("Hello world update 2!");
     }
 }
